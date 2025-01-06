8608460cba12c4bbb094bcabe9e9eeee3ea2f7fe
jmshima01 <jamesshima@mines.edu>
2025-01-05 22:58:56 -0700

    public

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# beatstars_plays_bot
+# Beatstars Views/Plays Bot
+A bot that continuously plays beat on beatstars for extra plays with no login required!
+
+# Usage:
+`python3 main.py <your_beat_url>`
---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+import re
+import json
+import random
+import sys
+import time
+def play_beat(beat):
+    
+    url = "https://main.v2.beatstars.com/stats/track_play"
+    valid_beat_url = re.search("https://www.beatstars.com/beat/.+-\d+", beat).group()
+    user_beat_url = ""
+    if valid_beat_url:
+        user_beat_id = re.search("\-\d+$",valid_beat_url).group()
+    else:
+        raise Exception("invaild beat url try again...")
+    
+    if not user_beat_id:
+        raise Exception("unable to parse beat id from url ending :( contact script owner")
+    data = {
+        "id": user_beat_id,
+        "sponsored":False,
+        "store_type": "<script>alert(1);</script>"
+    }
+    headers = {
+        "Host": "main.v2.beatstars.com",
+        "User-Agent": "<script>alert(1);</script>",}
+    print()
+    response = requests.post(url,json=data,headers=headers)
+
+    print(response.content)
+    
+if __name__ == "__main__":
+    url = "https://main.v2.beatstars.com/stats/track_like"
+
+    beat = ""
+    if len(sys.argv) != 2:
+        raise Exception("usage: python3 main.py <beat_url>")
+    beat = sys.argv[1]
+    while 1:
+        play_beat(beat)
+        time.sleep(random.randint(0,2))
