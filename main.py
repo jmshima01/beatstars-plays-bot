6b4e01572320a3b3db442405da64dcb5a7a0b565
jmshima01 <jamesshima@mines.edu>
2025-01-05 23:01:25 -0700

    clean

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 import re
-import json
 import random
 import sys
 import time
+
+# Beatstars Plays B0t (not a DOS!)
 def play_beat(beat):
-    
     url = "https://main.v2.beatstars.com/stats/track_play"
     valid_beat_url = re.search("https://www.beatstars.com/beat/.+-\d+", beat).group()
     user_beat_url = ""
@@ -26,16 +26,12 @@ def play_beat(beat):
         "User-Agent": "<script>alert(1);</script>",}
     print()
     response = requests.post(url,json=data,headers=headers)
-
     print(response.content)
     
 if __name__ == "__main__":
-    url = "https://main.v2.beatstars.com/stats/track_like"
-
-    beat = ""
     if len(sys.argv) != 2:
         raise Exception("usage: python3 main.py <beat_url>")
     beat = sys.argv[1]
     while 1:
         play_beat(beat)
-        time.sleep(random.randint(0,2))
+        time.sleep(random.randint(0,2)) # do not DOS!!
